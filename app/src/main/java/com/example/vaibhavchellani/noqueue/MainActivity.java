f3cb211a01b91bd981ac905acb12640949ac9c47
vaibhavchellani <vaibhavchellani223@gmail.com>
2017-09-28 17:31:12 +0530

    creating basic fucntionality

--- app/src/main/java/com/example/vaibhavchellani/noqueue/MainActivity.java
+++ app/src/main/java/com/example/vaibhavchellani/noqueue/MainActivity.java
@@ -1,45 +1,35 @@
 package com.example.vaibhavchellani.noqueue;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private Button trial_createqueue;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextMessage =  findViewById(R.id.message);
+        trial_createqueue=findViewById(R.id.create);
+        trial_createqueue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+            }
+        });
+
     }
 
 }
