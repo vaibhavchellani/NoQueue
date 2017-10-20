ca7c05b5d9012931dca987cdb22ef59fd92e0897
vaibhavchellani <vaibhavchellani223@gmail.com>
2017-10-20 17:15:13 +0530

    minor fixes

--- app/src/main/java/com/example/vaibhavchellani/noqueue/JoinQueue.java
+++ app/src/main/java/com/example/vaibhavchellani/noqueue/JoinQueue.java
@@ -1,9 +1,13 @@
 package com.example.vaibhavchellani.noqueue;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.ContactsContract;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,6 +19,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.sql.Time;
 
 import Models.Token;
 import butterknife.BindView;
@@ -40,19 +46,44 @@ public class JoinQueue extends AppCompatActivity {
         //replace the child id with the one we get from recycler view
         // TODO : queue_id to be replaced
         final String queue_id="-KwFoUxtH-JKChRWDy5f";
+        //TODO : user id to be replaced
+        final String user_id="-KwFpT0L-T1SC0PU34cE";
         final DatabaseReference queue_ref=mdatabase.child("queues").child(queue_id);
         final int[] no_of_users = new int[1];
         final int[] latest_token = new int[1];
+        //added demo working of current token and need queue start date and time to make it work properly
+        final int[] current_token = new int[1];
         //no idea why have used value event listener instead of child event listener , if someone feels child event listener is better create an issue
         queue_ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                Toast.makeText(JoinQueue.this, String.valueOf(dataSnapshot), Toast.LENGTH_SHORT).show();
-                
+
                 //// TODO: 10/12/17 get all the info about the queue and populate front end with them , see example below
                 mTextView1.setText(dataSnapshot.child("name_of_queue").getValue(String.class));
-                 no_of_users[0] =dataSnapshot.child("no_of_tokens").getValue(Integer.class);
+                no_of_users[0] =dataSnapshot.child("no_of_tokens").getValue(Integer.class);
                 latest_token[0]=dataSnapshot.child("latest_token").getValue(Integer.class);
+                // time of queue start time
+                final String time = dataSnapshot.child("queue_start_time").getValue(String.class);
+                // need to compare current time and queue start time to distribute token from starting time of queue
+                if(System.currentTimeMillis() >= Long.parseLong("1508348152055")){
+                    current_token[0]=dataSnapshot.child("current_token").getValue(Integer.class);
+                }
+                //Next Button on admin panel to be attached here
+
+                //queue_ref.child("current_token").setValue(current_token[0]+1);
+
+                if(current_token[0]<latest_token[0]){
+                    queue_ref.child("current_token").setValue(current_token[0]+1);
+                   if( dataSnapshot.child("users").child(user_id).child("token_no").getValue(Integer.class) == current_token[0]){
+                       queue_ref.child("users").child(user_id).child("token_status").setValue(0);
+
+                   }
+                    current_token[0]++;
+
+                }
+
+                Log.e("current token",String.valueOf(current_token[0]));
+
             }
             @Override
             public void onCancelled(DatabaseError databaseError) {
@@ -93,7 +124,7 @@ public class JoinQueue extends AppCompatActivity {
                 queue_ref.child("latest_token").setValue(latest_token[0]+1);
                 queue_ref.child("no_of_tokens").setValue(no_of_users[0]+1);
 
-                //Todo redirect user to mainActivity
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
             }
         });
 
