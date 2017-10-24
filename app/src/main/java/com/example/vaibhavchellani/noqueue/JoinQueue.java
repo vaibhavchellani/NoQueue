cff460ac2b253145195aa0cc823fa5db2d349b55
Danilo Lemes <daniloleemes@hotmail.com>
2017-10-24 21:45:44 -0200

    update gitignore

--- app/src/main/java/com/example/vaibhavchellani/noqueue/JoinQueue.java
+++ app/src/main/java/com/example/vaibhavchellani/noqueue/JoinQueue.java
@@ -4,16 +4,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
-import android.provider.ContactsContract;
-import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -21,8 +16,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.sql.Time;
 
 import Models.Token;
 import butterknife.BindView;
@@ -35,23 +28,27 @@ import butterknife.ButterKnife;
 
 public class JoinQueue extends AppCompatActivity {
 
-    @BindView(R.id.textView1) TextView mTextView1;
-    @BindView(R.id.textView2) TextView mTextView2;
-    @BindView(R.id.enrollButton) Button enrollButton;
+    @BindView(R.id.textView1)
+    TextView mTextView1;
+    @BindView(R.id.textView2)
+    TextView mTextView2;
+    @BindView(R.id.enrollButton)
+    Button enrollButton;
     private DatabaseReference mdatabase;
     SharedPreferences msharedPrefs;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.joinqueue);
         ButterKnife.bind(this);
-        mdatabase= FirebaseDatabase.getInstance().getReference();
-        msharedPrefs=getSharedPreferences("NoQueue", Context.MODE_PRIVATE);
+        mdatabase = FirebaseDatabase.getInstance().getReference();
+        msharedPrefs = getSharedPreferences("NoQueue", Context.MODE_PRIVATE);
         //replace the child id with the one we get from recycler view
         // TODO : queue_id to be replaced
-        final String queue_id="-KwtZ07j-UBk9vssAo6w";
-        final String user_id=msharedPrefs.getString(queue_id,"");
-        final DatabaseReference queue_ref=mdatabase.child("queues").child(queue_id);
+        final String queue_id = "-KwtZ07j-UBk9vssAo6w";
+        final String user_id = msharedPrefs.getString(queue_id, "");
+        final DatabaseReference queue_ref = mdatabase.child("queues").child(queue_id);
         final int[] no_of_users = new int[1];
         final int[] latest_token = new int[1];
         //added demo working of current token and need queue start date and time to make it work properly
@@ -63,11 +60,12 @@ public class JoinQueue extends AppCompatActivity {
 
                 //// TODO: 10/12/17 get all the info about the queue and populate front end with them , see example below
                 mTextView1.setText(dataSnapshot.child("name_of_queue").getValue(String.class));
-                no_of_users[0] =dataSnapshot.child("no_of_tokens").getValue(Integer.class);
-                latest_token[0]=dataSnapshot.child("latest_token").getValue(Integer.class);
+                no_of_users[0] = dataSnapshot.child("no_of_tokens").getValue(Integer.class);
+                latest_token[0] = dataSnapshot.child("latest_token").getValue(Integer.class);
 
 
             }
+
             @Override
             public void onCancelled(DatabaseError databaseError) {
             }
@@ -81,15 +79,19 @@ public class JoinQueue extends AppCompatActivity {
                  */
 
             }
+
             @Override
             public void onChildChanged(DataSnapshot dataSnapshot, String s) {
             }
+
             @Override
             public void onChildRemoved(DataSnapshot dataSnapshot) {
             }
+
             @Override
             public void onChildMoved(DataSnapshot dataSnapshot, String s) {
             }
+
             @Override
             public void onCancelled(DatabaseError databaseError) {
             }
@@ -100,17 +102,17 @@ public class JoinQueue extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Token newToken=new Token(latest_token[0]+1);
-                DatabaseReference setTokenRef=queue_ref.child("users").push();
+                Token newToken = new Token(latest_token[0] + 1);
+                DatabaseReference setTokenRef = queue_ref.child("users").push();
                 //todo save the "setTokenRef" in sharedPrefs with the respective queue id
-                SharedPreferences.Editor editor =msharedPrefs.edit();
-                editor.putString(queue_id,setTokenRef.toString());
+                SharedPreferences.Editor editor = msharedPrefs.edit();
+                editor.putString(queue_id, setTokenRef.toString());
                 editor.commit();
                 setTokenRef.setValue(newToken);
-                queue_ref.child("latest_token").setValue(latest_token[0]+1);
-                queue_ref.child("no_of_tokens").setValue(no_of_users[0]+1);
+                queue_ref.child("latest_token").setValue(latest_token[0] + 1);
+                queue_ref.child("no_of_tokens").setValue(no_of_users[0] + 1);
 
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
 
             }
         });
